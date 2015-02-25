646e79137413b0ca8c026c8070d6bf551c82cfd3
Austin Hartzheim <austin@austinhartzheim.me>
2015-02-25 22:49:23 +0000

    Added state tracking via CSV output.
    A CSV output file is placed in the data directory with the timestamp of
    the start time in the file name. This tracks all data going through, and
    records modifications to the 'state' and 'last contacted' fields.
    
    I also made the data validation in the Sponsor class more strict and
    converted the main() fuction into the Main() class.
    
    Some new data validation code is included in libs/validate.py and you
    can run this code by running `python madhacksbot.py --validate /path/to.csv`

--- madhacksbot.py
+++ madhacksbot.py
@@ -1,23 +1,26 @@
 #! /usr/bin/python
 __author__ = 'riccardo mutschlechner'
 
-import smtplib # SMTP email
-import csv # CSV parsing
-import re # regex
-from email.mime.multipart import MIMEMultipart # moar SMTP email
-from email.mime.text import MIMEText # even moar SMTP email
-from keys import getkey, getlogin # login/pass for email acct, needs to be created locally
+import argparse
+import smtplib  # SMTP email
+import csv  # CSV parsing
+import re  # regex
+from email.mime.multipart import MIMEMultipart  # moar SMTP email
+from email.mime.text import MIMEText  # even moar SMTP email
+from keys import getkey, getlogin  # login/pass for email acct, needs to be created locally
+import time
 
 COL_COMPANY_NAME = 0
 COL_CONTACT_NAME = 1
 COL_CONTACT_EMAIL = 3
-COL_STATUS = 5  # TODO: check row status before sending
+COL_STATUS = 5
+COL_LAST_CONTACTED = 7
 
-class Sponsor:
+class Sponsor():
     # Some of these are valid in emails, but we should be careful
-    bad_characters = (';', '/', '\\', '"', "'")
+    bad_characters = (';', '/', '\\', '"', "'", '(', ')')
 
-    def __init__(self, name, contactName, email, tier):
+    def __init__(self, name, contactName, email, tier=None):
         self.name = self.__validate(name)
         self.contactName = self.__validate(contactName)
         self.email = self.__validate(email)
@@ -32,13 +35,8 @@ class Sponsor:
         return ('%s: %s, %s, %s' % (self.name, self.contactName. self.email,
                 self.tier))
 
-
-    def printsponsor(self):
-        print self.name, " ", self.contactName, " ", self.email, " ", self.tier
-
-
     def sendemail(self, content):
-        print "Sending email to: ", self.name
+        print("Sending email to: ", self.name)
         # me == my email address
         # you == recipient's email address
         me = "team@madhacks.org"
@@ -60,13 +58,13 @@ class Sponsor:
         # the HTML message, is best and preferred.
         msg.attach(part1)
 
-        user = getlogin() # this function needs to be replaced locally (or defined) with the email login.
-        passwd = getkey() # this function needs to be replaced locally (or defined) with the email password.
+        user = getlogin()  # this function needs to be replaced locally (or defined) with the email login.
+        passwd = getkey()  # this function needs to be replaced locally (or defined) with the email password.
         print("Sending...")
 
         # Send the message via local SMTP server.
         s = smtplib.SMTP_SSL('smtp.gmail.com')
-        s.login(user,passwd)
+        s.login(user, passwd)
 
         # sendmail function takes 3 arguments: sender's address, recipient's address
         # and message to send - here it is sent as one string.
@@ -82,12 +80,11 @@ class Sponsor:
                 raise ValueError("Bad character '%s' found in text." % char)
         return text.strip()
 
-
     def buildemail(self):
         print('Building email for Sponsor: "%s", "%s", "%s"' %
               (self.name, self.contactName, self.email))
         filename = (self.name + ".txt").replace(" ", "_")
-        filename = filename.replace("/", "") # strip bad stuff for files
+        filename = filename.replace("/", "")  # strip bad stuff for files
         filename = "./emails/" + filename
 
         template = ""
@@ -107,9 +104,7 @@ class Sponsor:
 
         return template
 
-
-
-    def confirmSend(self, template):
+    def confirm_send(self, template):
         print("ARE YOU SURE YOU WOULD LIKE THE BOT TO SEND THE EMAIL TO: %s %s" %
               (self.name, self.contactName))
         try:
@@ -122,16 +117,29 @@ class Sponsor:
             return False
 
 
-def main():
-    Sponsors = []
-    with open('data/sponsors_test.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+class Main():
+    def send_emails(self, args):
+        sponsors = []
+        reader = csv.reader(args.file, delimiter=',')
+
+        outfile = open('data/sponsors-run-%i.csv' % time.time(), 'w')
+        writer = csv.writer(outfile)
+
         for row in reader:
-            # Check for malformated rows
+            # Check conditions where we shouldn't send
             if len(row) < 3 or row[COL_COMPANY_NAME] == 'Company Name':
+                writer.writerow(row)
+                continue
+            elif row[COL_STATUS] != 'Not Contacted':
+                print('Skipping row due to status field')
+                writer.writerow(row)
+                continue
+            elif row[COL_LAST_CONTACTED]:
+                print('Skipping row due to last contact date.')
+                writer.writerow(row)
                 continue
 
-            # when we add a tier field, if we do, please change [[TIER]] to the appropriate field
+            # Load other columns
             name = row[COL_COMPANY_NAME]
             if len(name) < 3:
                 name = None
@@ -141,31 +149,47 @@ def main():
                 contactName = None
 
             email = row[COL_CONTACT_EMAIL]
-            # make sure email is valid, and also a valid length
             if len(email) < 5 or not re.match("[^@()]+@[\w\-]+\.[\w]+", email):
                 email = None
 
+            sponsor = Sponsor(name, contactName, email)
 
-            tier = ""
-            if len(tier) < 3:
-                tier = None
-
-            Sponsors.append(Sponsor(name, contactName, email, tier))
-
-        for sponsor in Sponsors:
-            # print(sponsor)
-
-            # if we have all of the correct fields... build the emails!
+            # Final check, and then ask to send
             if sponsor.name and sponsor.contactName and sponsor.email:
-                template = sponsor.buildemail()
-                # Confirm sending with user
-                if sponsor.confirmSend(template):
-                    sponsor.sendemail(template)
-                else:
-                    print("Chose not to send email to " + sponsor.name)
+                email_sent = self.send_email(sponsor)
+
+                if email_sent:
+                    now = time.gmtime()
+                    date = '%i/%i/%i' % (now.tm_mon, now.tm_mday, now.tm_year)
+                    row[COL_STATUS] = 'Waiting Response'
+                    row[COL_LAST_CONTACTED] = date
             else:
-                print("Cannot build email for sponsor because of missing or invalid fields")
+                print('Cannot build email; field data is missing/invalid')
+
+            writer.writerow(row)
+
+        outfile.close()
+
+    def send_email(self, sponsor):
+        template = sponsor.buildemail()
+
+        if sponsor.confirm_send(template):
+            sponsor.sendemail(template)
+            return True
+        else:
+            print('Chose not to send email to %s/%s' %
+                  (sponsor.name, sponsor.get_first_name()))
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--validate', action='store_true')
+    parser.add_argument('file', type=argparse.FileType('r'))
+    args = parser.parse_args()
+
+    if args.validate:  # User passed the --validate option
+        import libs.validate
+        libs.validate.validate(args)
+    else:
+        main = Main()
+        main.send_emails(args)
